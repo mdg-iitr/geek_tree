2491e06dbc838f60a5e9b5a2be37ece4a1821254
KaairaGupta <kgupta@es.iitr.ac.in>
2018-12-12 00:43:16 +0530

    Designed navigation and signup, registration UI completed

---
+++ app/src/main/java/com/codaira/geektree/Login.kt
@@ -0,0 +1,24 @@
+package com.codaira.geektree
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.codaira.geektree.R.id.*
+import kotlinx.android.synthetic.main.fragment_login.*
+
+class Login : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+
+}
